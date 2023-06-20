77cce173b756865116cc5870f2a748d0c5dac3a9
Philipp Kotte <philippkotte@icloud.com>
2023-06-20 14:18:40 +0200

    Front Page

--- DB-Tracker/ContentView.swift
+++ DB-Tracker/ContentView.swift
@@ -9,11 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("DB-tacker")
-                .monospaced()
-        }.frame(width: 400, height: 600)
-        .padding()
+       
+        
+            ZStack{
+                 Color("DB_RED")
+                VStack {
+                    Text("DB-tacker")
+                        .font(.largeTitle)
+                        .fontWeight(.bold)
+                        .foregroundColor(Color("DB_WHITE"))
+                        .monospaced()
+                    Spacer()
+                }.frame(width: 400, height: 600)
+                    .padding(.vertical, 10)
+            }
     }
 }
 
