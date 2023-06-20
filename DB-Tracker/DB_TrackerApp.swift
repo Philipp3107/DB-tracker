24812ac7703ac026cb2688b626e7a2cb08875e37
Philipp Kotte <philippkotte@icloud.com>
2023-06-20 13:53:54 +0200

    Initial Commit

---
+++ DB-Tracker/DB_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DB_TrackerApp.swift
+//  DB-Tracker
+//
+//  Created by Philipp Kotte on 20.06.23.
+//
+
+import SwiftUI
+
+@main
+struct DB_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
